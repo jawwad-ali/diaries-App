ccfedd20bea45b46dada1f18769d92c07e4fce77
jawwad-ali <alijawwad001@gmail.com>
2020-11-25 23:21:14 +0500

    first commit

---
+++ src/services/mirage/routes/user.tsx
@@ -0,0 +1,55 @@
+import { randomBytes } from 'crypto';
+import { Request, Response } from 'miragejs';
+import { User } from '../../../interfaces/user.interface';
+import { handleErrors } from '../server';
+
+const generateToken = () => randomBytes(8).toString('hex');
+
+export interface AuthResponse {
+    token: string,
+    user: User
+}
+
+// LOGIN
+const login = (schema: any, req: Request): AuthResponse | Response => {
+
+    const { username, password } = JSON.parse(req.requestBody)
+    const user = schema.users.findBy({ username })
+
+    if (!user) {
+        return handleErrors(null, "No user with that username exists");
+    }
+    if (password !== user.password) {
+        return handleErrors(null, "password is incorrect")
+    }
+
+    const token = generateToken()
+    return {
+        user: user.attrs as User,
+        token
+    }
+}
+
+// SIGN UP
+const signup = (schema: any, req: Request): AuthResponse | Response => {
+    const data = JSON.parse(req.requestBody);
+    const exUser = schema.users.findBy({ username: data.username });
+
+    if (exUser) {
+        return handleErrors(null, "Username already exists")
+    }
+
+    const user = schema.users.create(data)
+    const token = generateToken()
+
+    return {
+        user: user.attrs as User,
+        token
+    }
+
+}
+
+export default {
+    login,
+    signup,
+};
